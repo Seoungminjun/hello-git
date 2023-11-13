59a28685679ad2b713c116646814f2c0872fe094
Seoungminjun <dkwj000@naver.com>
2023-11-13 11:22:03 +0900

    Main.main 대문자로  수정

--- src/com/sh/app/Main/Main.java
+++ src/com/sh/app/Main/Main.java
@@ -1,9 +1,18 @@
 package com.sh.app.Main;
 
+import com.sh.app.animal.Cat;
+import com.sh.app.animal.Dog;
+import com.sh.app.animal.Tiger;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		System.out.println("HELLO WORLD");
+		System.out.println("HELLO GIT");
+		
+		new Dog().bark();
+		new Cat().jump();
+		new Tiger().stack();
 	}
 
 }
